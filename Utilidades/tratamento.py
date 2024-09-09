652961b82a10a30d52f9ae58667f750db8ff81fe
Rodrigo-Pechini <rodrigopechini01@gmail.com>
2024-09-09 15:37:52 -0300

    Add comentarios

--- Utilidades/tratamento.py
+++ Utilidades/tratamento.py
@@ -1,14 +1,13 @@
-def contagemInvertida(inicio, fim):
+def contagemInvertida(inicio, fim): # Avaliando se a contagem vai ser invertida
     if inicio > fim:
         return -1
     else:
         return 1
     
-def valorInteiro(msg):
+def valorInteiro(msg):# Verificação da resposta do user
     while True:
         try:
             valor = int(input(msg))
             return valor
         except ValueError:
             print('\033[31mERRO!! Valor invalido digite novamente.\033[m')
-
--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from time import sleep
 from Utilidades import tratamento
 
 print('ENCONTRANDO NUMEROS \033[32mPARES\033[m')
+
+# Interação com usuario é avaliando resposta
 inicio = tratamento.valorInteiro('Digite o valor inicial da contagem: ')
 fim = tratamento.valorInteiro('Digite o valor final da contagem: ')
 
+# Avaliando se a contagem vai ser invertida
 paso = tratamento.contagemInvertida(inicio, fim)
 if paso == -1:
     fim -= 2
 
+# Mostrando o resultado
 for n in range(inicio, fim + 1, paso):
     if n % 2 == 0:
         print(f'\033[32m{n}\033[m', end=' ')
