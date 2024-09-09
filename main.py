06e5c0f9eb025a6da6096f864bc040d36cb3dcb9
Rodrigo-Pechini <rodrigopechini01@gmail.com>
2024-09-09 11:07:06 -0300

    add tratamentos
    Foi adicionado um tratamento para indicar se ele é ou não um numero e foi adicionado uma contagem invertida

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+from Utilidades import tratamento
+
 print('ENCONTRANDO NUMEROS PARES')
 
-inicio = int(input('Digite o inicio da contagem: '))
-fim = int(input('Digite o fim da contagem: '))
+inicio = tratamento.valorInteiro('Digite o valor inicial da contagem: ')
+fim = tratamento.valorInteiro('Digite o valor final da contagem: ')
 
-for n in range(inicio, fim + 1):
+paso = tratamento.contagemInvertida(inicio, fim)
+if paso == -1:
+    fim -= 2
+
+for n in range(inicio, fim + 1, paso):
     if n % 2 == 0:
         print(f'\033[32m{n}\033[m', end=' ')
     else:
         print(n, end=' ')
+print()
 print('Programa finalizado')
